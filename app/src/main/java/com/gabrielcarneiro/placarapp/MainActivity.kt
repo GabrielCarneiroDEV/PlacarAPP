29ec63e4a08256bae54bd4e516a52f1bae27aadc
Gabriel Carneiro <gabriel29music@gmail.com>
2021-01-01 16:56:08 -0300

    Placar futebol pronto

--- app/src/main/java/com/gabrielcarneiro/placarapp/MainActivity.kt
+++ app/src/main/java/com/gabrielcarneiro/placarapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.gabrielcarneiro.placarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var scoreTime1 = 0
+        var scoreTime2 = 0
+
+        scoreTime1TXT.setOnClickListener{
+            scoreTime1++
+            scoreTime1TXT.text = scoreTime1.toString()
+        }
+
+
+        scoreTime2TXT.setOnClickListener{
+            scoreTime2++
+            scoreTime2TXT.text = scoreTime2.toString()
+
+        }
     }
 }
